42dc496e55cbfce9f66a5788c5597004504001e6
nicolasgarciapinto00@gmail.com <nicolasgarciapinto00@gmail.com>
2023-02-17 11:14:06 +0000

    3

--- models/staging/bases/base_orders.sql
+++ models/staging/bases/base_orders.sql
@@ -1,5 +1,19 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key=["o_orderkey"]
+    )
+}}
+
+
 with a1 as (
     select * from {{ source('dbt_niqueton', 'orders') }}
 )
 
 select * from a1
+
+{% if is_incremental() %}
+
+where o_load_timestamp >= (select max(o_load_timestamp) from {{ this }})
+
+{% endif %}

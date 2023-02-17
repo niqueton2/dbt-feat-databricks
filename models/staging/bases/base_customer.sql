814a5550aac50cbe98c48e1d2cc8a391ce54e5c8
nicolasgarciapinto00@gmail.com <nicolasgarciapinto00@gmail.com>
2023-02-17 10:29:18 +0000

    e

--- models/staging/bases/base_customer.sql
+++ models/staging/bases/base_customer.sql
@@ -1,5 +1,20 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key=["c_custkey"]
+    )
+}}
+
+
 with a1 as (
     select * from {{ source('dbt_niqueton', 'customer') }}
 )
 
 select * from a1
+
+
+{% if is_incremental() %}
+
+where c_load_timestamp >= (select max(c_load_timestamp) from {{ this }})
+
+{% endif %}
--- models/staging/bases/base_part.sql
+++ models/staging/bases/base_part.sql
@@ -1,5 +1,19 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key=["p_partkey"]
+    )
+}}
+
+
 with a1 as (
     select * from {{ source('dbt_niqueton', 'part') }}
 )
 
 select * from a1
+
+{% if is_incremental() %}
+
+where p_load_timestamp >= (select max(p_load_timestamp) from {{ this }})
+
+{% endif %}
